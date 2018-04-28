3cbdfeaaffff807e96c73cfd80883be023d10aa1
Destry Saul <destrys@gmail.com>
2018-04-28 14:22:40 -0700

    init main contract

---
+++ contracts/InvalidOpcode.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.17;
+
+contract InvalidOpcode {
+
+  // Structs
+
+  struct Question {
+
+  }
+
+  struct Answer {
+    bool
+
+  }
+
+  
+  // Storage
+  Question[] public questions;
+  
+
+  function postQuestion() public {
+
+  }
+
+  function addBounty() public {
+
+  }
+
+  function postAnswer() public {
+
+  }
+
+  function upVote() public {
+
+  }
+
+  function downVote() public {
+
+  }
+
+  function claimBount() public {
+
+
+  }
+
+
+}
